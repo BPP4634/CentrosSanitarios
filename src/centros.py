9cffa7ac61000b03ea85523535cd1b2c3123a92f
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-10-19 09:28:36 +0200

    CentrosSanitarios

--- src/centros.py
+++ src/centros.py
@@ -25,10 +25,10 @@ def calcular_total_camas_centros_accesibles(centros):
         if centro.acceso_minusvalidos == True:
             camas.append(centro.num_camas)
     return sum(camas)
-'''3. obtener_centros_con_uci_cercanos_a: recibe una lista de tuplas de tipo CentroSanitario, una tupla
-de tipo Coordenada que representa un punto, y un float que representa un umbral de distancia, y
-produce como salida una lista de tuplas (str, str, Coordenada(float, float)) con el nombre, la localidad
-y las coordenadas de los centros situados a una distancia de las coordenadas dadas como parámetro
-menor o igual que el umbral dado. Observe la Figura 3 para entender mejor el resultado de la función.
-Figura 3: Radio de centros con UCI buscados
-'''
+
+def obtener_centros_con_uci_cercanos_a(centros, coor,n):
+    centroscerca = []
+    for centro in centros:
+        if calcular_distancia(centro.coordenada, coor) <=n:
+            centroscerca.append((centro.nombre,centro.localidad,centro.coordenada))
+    return centroscerca
--- src/centros_test.py
+++ src/centros_test.py
@@ -4,6 +4,7 @@ def main():
     DATOS = lee_datos('./data/centrosSanitarios.csv')
     print(DATOS[:3])
     print(calcular_total_camas_centros_accesibles(DATOS))
+    print(obtener_centros_con_uci_cercanos_a(DATOS,Coordenada(36,-1.4),200))
 
 if __name__ == '__main__':
     main()
