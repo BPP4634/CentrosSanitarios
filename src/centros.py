c9be8963027032d99edf6fe79195f3c5d767b8b7
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-10-19 16:27:16 +0200

    CentrosSanitarios

--- src/centros.py
+++ src/centros.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from coordenadas import *
+from mapas import *
 
 CentroSanitario = namedtuple('CentroSanitario', 'nombre, localidad, coordenada, estado, num_camas, acceso_minusvalidos, tiene_uci')
 
@@ -32,3 +33,37 @@ def obtener_centros_con_uci_cercanos_a(centros, coor,n):
         if calcular_distancia(centro.coordenada, coor) <=n:
             centroscerca.append((centro.nombre,centro.localidad,centro.coordenada))
     return centroscerca
+
+def generar_mapa(centros):
+    coor = []
+    for centro in centros:
+        coor.append(centro[2])
+    print(coor)
+    media = calcular_media_coordenadas(coor)
+    mapa = crea_mapa(media.latitud,media.longitud)
+    for centro in centros:
+        marcador = crea_marcador(centro[2].latitud,centro[2].longitud,centro[0])
+        marcador.add_to(mapa)
+    mapa.save('./out/mapa.html')
+
+'''4. generar_mapa: recibe una lista de tuplas (str, str, Coordenada(float, float)) con el nombre, la
+localidad y las coordenadas de los centros, y una cadena que representa la ruta de un fichero html,
+y genera y graba en este fichero un mapa con los centros geolocalizados.
+Para implementar la función generar_mapa ayúdese de las funciones auxiliares que se
+implementan en el módulo mapas.py. Además, tenga en cuenta que:
+1. Primero debe crear un mapa. Use la media de las coordenadas de los centros para centrar el
+mapa.
+2. Después debe ir creando marcadores y añadiéndolos al mapa. Una vez creado el marcador,
+use marcador.add_to(mapa) para añadirlo al mapa.
+3. Una vez añadidos todos los marcadores, guarde el mapa en el archivo html con
+mapa.save(fichero).
+El resultado deber ser un fichero con un mapa similar al de la Figura 4.
+Figura 4: Mapa con centros de salud geolocalizados
+RETO
+La fórmula de la distancia euclídea empleada en este proyecto no es la más adecuada cuando se quieren
+calcular distancias entre dos puntos del globo terrestre, ya que no tiene en cuenta la curvatura de la tierra.
+Para calcular la distancia entre dos puntos del globo terrestre se usa una aproximación que viene dada por
+la fórmula de Haversine1. Implemente una función distancia_harvesine para que en el proyecto se hagan
+unos cálculos más realistas.
+1 https://es.wikipedia.org/wiki/F%C3%B3rmula_del_haversine
+'''
--- src/centros_test.py
+++ src/centros_test.py
@@ -4,7 +4,9 @@ def main():
     DATOS = lee_datos('./data/centrosSanitarios.csv')
     print(DATOS[:3])
     print(calcular_total_camas_centros_accesibles(DATOS))
-    print(obtener_centros_con_uci_cercanos_a(DATOS,Coordenada(36,-1.4),200))
+    centroscercanos = (obtener_centros_con_uci_cercanos_a(DATOS,Coordenada(36,-1.4),200))
+    print(centroscercanos)
+    generar_mapa(centroscercanos)
 
 if __name__ == '__main__':
     main()
